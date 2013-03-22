912808c425ebaccba3671de1545116dfecfb7d2c
Scott Gill <scott500@wi.rr.com>
2013-03-21 23:37:27 -0500

    Drawing Simulator Finished. Action not started.
    Most of the work will be done by the action in this phase. The simulator
    is only responsible for generating text to spit out.

--- src/edu/uwm/JStateDrawer/DrawingSimulator.java
+++ src/edu/uwm/JStateDrawer/DrawingSimulator.java
@@ -1,71 +1,65 @@
 package edu.uwm.JStateDrawer;
 
 import java.util.LinkedList;
+import java.util.Scanner;
+import java.io.*;
 
 import org.jhotdraw.draw.Drawing;
 import org.jhotdraw.draw.Figure;
 
-import edu.uwm.JStateDrawer.figures.StartStateFigure;
 import edu.uwm.JStateDrawer.figures.StateFigure;
 import edu.uwm.JStateDrawer.figures.TransitionFigure;
 
 public class DrawingSimulator {
-	private LinkedList<StateFigure> statelist;
 	
-	private Drawing view;
+	public DrawingSimulator(){}
 	
-	public DrawingSimulator(){
-		view = null;
-		statelist = new LinkedList<StateFigure>();
-	}
-	
-	public DrawingSimulator(Drawing x){
-		view = x;
-		statelist = new LinkedList<StateFigure>();
-		
-		for (Figure s : view.getFiguresFrontToBack()){
-			if (s instanceof StateFigure) statelist.add((StateFigure)s);
-		}
-	}
-	
-	public String simulate(){
+	public String simulate(Drawing view) throws FileNotFoundException{
 		String r = "";
 		
-		boolean end = false;
+		LinkedList<StateFigure> statelist = new LinkedList<StateFigure>();
+		for(Figure s : view.getFiguresFrontToBack()){
+			if (s instanceof StateFigure) statelist.add((StateFigure) s);
+		}
+		
 		
 		String e = "";
 		
 		StateFigure currentState = null;
-		
-		if (view != null){
-			if (new DrawingChecker().validateCurrentDrawing(view)){
-				for (StateFigure s : statelist){
-					if (s instanceof StartStateFigure){
-						currentState = s;
-						break;
-					}
+
+		if (new DrawingChecker().validateCurrentDrawing(view)){
+			Scanner f = new Scanner(new FileReader("INPUT"));
+			for (StateFigure s : statelist){
+				if (s.getName().equals("Start")){
+					currentState = s;
+					break;
 				}
-				while(true){
-					//e = stringReadFromFile
-					if (currentState.getModel().getAllActions().containsValue(e)) r += "Action Performed: " + e + "\n";
+			}
+			r += currentState.getOutgoingTransitions().iterator().next().getModel().getTrigger();
+			r += " to " + currentState.getOutgoingTransitions().iterator().next().getEndStateFigure().getName() + "\n";
+			currentState = currentState.getOutgoingTransitions().iterator().next().getEndStateFigure();
+			for (String s : currentState.getModel().getActionsByTrigger("ENTRY")) r += "Entering action: " + s + "\n";
+			while(true){
+				e = f.next();
+				if (currentState.getModel().getAllActions().containsValue(e)) {
+					r += "Action Performed: " + e + "\n";
 					for (TransitionFigure s : currentState.getOutgoingTransitions()){
-						if (currentState.getModel().getActionsByTrigger(s.getModel().getTrigger()) != null){
+						if (currentState.getModel().getActionsByTrigger(s.getModel().getTrigger()).contains(e)){
+							for (String d : currentState.getModel().getActionsByTrigger("EXIT")) r += "Exiting action: " + d + "\n";
 							currentState = s.getEndStateFigure();
 							r += "Transition to: " + currentState.getName() + "\n";
+							for (String d : currentState.getModel().getActionsByTrigger("ENTRY")) r += "Entering action: " + d + "\n";
 							break;
 						}
 					}
-					if (currentState.getName().equals("End")) break;
 				}
-			}
-			else{
-				
+				if (currentState.getName().equals("End")) break;
 			}
 		}
 		else{
-			
+			return r;
 		}
-		
+		f.close();
 		return r;
 	}
 }
